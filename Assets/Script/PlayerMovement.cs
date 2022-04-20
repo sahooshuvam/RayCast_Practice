0858e0183f2af0527834eb2f0ec13a8bc281598e
Shuvam Sahoo <shuvam.sahoo@yesgnome.com>
2022-04-20 12:46:38 +0530

    Obstacle Created.... On Hitting that obstacle by Raycast... and on Hitting Obstacle will Destroyed...

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float playerSpeed = 8f;
+    public Transform rayPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +21,23 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * xInput + transform.forward * zInput;
         controller.Move(move * playerSpeed * Time.deltaTime);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            HitObstacle();
+        }
+    }
+
+    private void HitObstacle()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(rayPoint.position, rayPoint.forward, out hitInfo, 100f))
+        {
+            GameObject hitObstacle = hitInfo.collider.gameObject;
+            if (hitObstacle.tag == "Obstacle")
+            {
+                Destroy(hitObstacle);
+            }
+        }
     }
 }
